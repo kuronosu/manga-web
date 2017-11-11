eec07d9b88b7f77f503edd3f09cb7bca48353457
Felipe Martinez <andresfelipe.2031@gmail.com>
2017-11-10 22:19:58 -0500

    Mejorando los formulaios

--- manageManga/forms.py
+++ manageManga/forms.py
@@ -9,54 +9,43 @@ class MangaRegistrationForm(forms.ModelForm):
         super(MangaRegistrationForm, self).__init__(*args, **kwargs)    
         for field in self.fields:
             self.fields[field].widget.attrs = {
-                'class': 'form-control', 
-                'required': True
+                'class': 'form-control',
+                'required': True,
             }
+        self.fields['title'].widget.attrs['placeholder'] = _('Titulo del manga')
+        self.fields['description'].widget.attrs['placeholder'] = _('Description del manga')
+        self.fields['genres'].widget.attrs = {'onchange': 'validacion()'}
 
     class Meta:
         model = Manga
-        # fields = '__all__'
         fields = ['title', 'description', 'state', 'genres']
-        widgets = {
-            'title': forms.TextInput(
-                attrs = {'placeholder': 'Titulo del manga'}
-            ),
-        }
-        # self.fields['title'].widget.attrs = {
-        #     'placeholder': 'Titulo del manga'
-        # }
-        # self.fields['description'].widget.attrs = {
-        #     'placeholder': 'Algo aquí'
-        # }
-        # self.fields['state'].widget.attrs = {
-        #     'placeholder': 'Algo más por aquí'
-        # }
+        widgets = {'genres': forms.CheckboxSelectMultiple()}
 
-        # widgets = {
-        #     'title': forms.TextInput(
-        #         attrs={'id': 'manga-text', 'required': True, 'placeholder': 'Titulo del manga'}
-        #     ),
-        # }
     def clean(self):
         genres = self.cleaned_data.get('genres')
         max_genres = 7
-        if len(genres) > max_genres:
-            # Translators: Mensaje que aparece cuando se intenta crear un manga con mas de un numero especifico de generos
-            self.add_error('genres', ugettext_lazy('Select maximum {} geners'.format(max_genres)))
-        return self.cleaned_data
+        if genres:
+            if len(genres) > max_genres:
+                # Translators: Mensaje que aparece cuando se intenta crear un manga con mas de un numero especifico de generos
+                self.add_error('genres', _('Select maximum {} geners'.format(max_genres)))
+            return self.cleaned_data
 
-class FilterForm(forms.Form):
+class FilterForm(forms.ModelForm):
     class Meta:
+        model = Manga
         fields = ['genres', 'state']
-    
-    genres = forms.MultipleChoiceField(
-        choices = Genre.GENRE_CHOICES,
-        widget  = forms.CheckboxSelectMultiple,
-    )
-    state = forms.ChoiceField(choices = State.STATE_CHOICES,)
+        widgets = {'genres': forms.CheckboxSelectMultiple()}
 
     def __init__(self, *args, **kwargs):
         super(FilterForm, self).__init__(*args, **kwargs)
-        self.fields['Genres'].widget.attrs = {
-            'onchange': 'validacion()'
-            }
+        self.fields['state'].required = False
+        self.fields['genres'].widget.attrs = {'onchange': 'validacion()'}
+
+    def clean(self):
+        genres = self.cleaned_data.get('genres')
+        max_genres = 7
+        if genres:
+            if len(genres) > max_genres:
+                # Translators: Mensaje que aparece cuando se intenta filtar con mas de un numero especifico de generos
+                self.add_error('genres', _('Select maximum {} geners'.format(max_genres)))
+            return self.cleaned_data
