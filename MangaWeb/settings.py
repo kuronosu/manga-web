f0943db479bc65824c48de3a8a7e8e02864931b3
KR0N0S31 <andresfelipe.2031@gmail.com>
2018-03-08 19:36:59 -0500

    Bootstrap implementado en navbar y pagina de login

--- MangaWeb/settings.py
+++ MangaWeb/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+try:
+    SECRET_KEY = os.environ['SECRET_KEY']
+except Exception:
+    # Poner la SECRET_KEY en el arcuvi de configuracion local
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
